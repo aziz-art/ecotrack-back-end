5cf8294d905d578aad01967a9a358deb047b4f70
unknown <maherbaldi969>
2025-04-20 21:46:55 +0100

    connection a distant

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 module.exports = {
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.DB_HOST || '86.75.61.19',
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'ecotrack',
+  database: process.env.DB_NAME || 'ecotrack_wordpress',
   port: process.env.DB_PORT || 3306,
 };
