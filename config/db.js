18ba2ec8d0eeb78184e7d2a1be342e0cdf43f0b7
unknown <maherbaldi969>
2025-04-21 16:27:26 +0100

    connection base

--- config/db.js
+++ config/db.js
@@ -1,6 +1,8 @@
 const mysql = require('mysql2/promise');
 
 // Configure with your WordPress database credentials
+//CREATE USER 'chaibiazi'@'%' IDENTIFIED BY 'chaibiazi2025';
+//CREATE USER 'AgurbiWad'@'%' IDENTIFIED BY 'AgurbiWad2025';
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
